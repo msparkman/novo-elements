6af0e5debeb5b29eb2f5ec3a14ba4556fabe6836
Joshua Godi <jgodi@bullhorn.com>
2016-08-04 13:43:25 -0500

    Updates

--- demo.28f6450bd8cd497dff39.bundle.js
+++ demo.28f6450bd8cd497dff39.bundle.js
@@ -173,7 +173,7 @@ webpackJsonp([34],{
 	
 	        this.router = router;
 	        this.menuOpen = false;
-	        this.version = ("0.2.7");
+	        this.version = ("0.2.8");
 	
 	        toastService.parentViewContainer = view;
 	        modalService.parentViewContainer = view;
